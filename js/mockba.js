d7ab53cdf9d57df21655571f28b6dbcf8d09cd51
Mockba the Borg <mockba.the.borg@gmail.com>
2025-08-16 08:28:17 -0400

    removed submit to front

--- js/mockba.js
+++ js/mockba.js
@@ -404,10 +404,6 @@ app.registerExtension({
 					app.queuePrompt(0); // 0 = add to end of queue, -1 = add to front
 				});
 
-				// Optional: Add a "Submit to Front" button for priority execution
-				const submitFrontWidget = this.addWidget("button", "Submit to Front", "Submit to Front", () => {
-					app.queuePrompt(-1); // -1 = add to front of queue
-				});
 
 				// Add reset canvas position button
 				const resetCanvasWidget = this.addWidget("button", "Reset Canvas", "Reset Canvas", () => {
